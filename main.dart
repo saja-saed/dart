4a26b4176621b2b080bcc11f7a17f26f8effcb71
saja-saed <96354185+saja-saed@users.noreply.github.com>
2022-02-23 15:17:18 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            title: Center(
+              child: Text("Home Screen"),
+            ),
+          ),
+          body: Container(
+              width: 350.0,
+              height: 100.0,
+              color: Colors.white,
+              child: Text("container app",
+                  textAlign: TextAlign.left,
+                  style: new TextStyle(
+                    color: Colors.pinkAccent,
+                    fontSize: 50,
+                  ))))));
+
+  Center(
+    child: Container(alignment: Alignment.centerLeft, color: Colors.pinkAccent),
+  );
+}
